46e2e7f240fd569ae8affe67919bcb67c01eee77
LeXarDev <kfo@usa.com>
2024-01-06 04:10:45 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['ForceDNS'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
