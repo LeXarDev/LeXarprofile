28f383fc5c4c8a36f41c91cfcd630d01af3d4eac
LeXarDev <kfo@usa.com>
2024-01-06 03:51:12 +0300

    Build

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,31 +2,21 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'LeXarDev', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['ForceDNS'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
+    linkedin: 'LeXarDev',
+    twitter: 'LeXarDev',
     instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
+    website: 'https://lexardev.com',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'khalid@lexardev.com',
   },
   resume: {
     fileUrl:
@@ -74,26 +64,20 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'University of Bisha',
+      degree: 'Computer science',
+      from: '2020',
+      to: '2025',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'ForceDNS',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
+        'ForceDNS is a specialized program designed for DNS (Domain Name System) management. This tool empowers users to manipulate and control DNS settings with ease. In addition to its primary function of applying DNS configurations, ForceDNS also provides the capability to discern the network activity of various programs.',
+      imageUrl: 'https://www.upload.ee/thumb/16130448/network_5.png',
       link: 'https://example.com',
     },
     {
@@ -119,7 +103,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'Night',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -134,8 +118,8 @@ const config = {
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
-      'light',
       'dark',
+      'light',
       'cupcake',
       'bumblebee',
       'emerald',
@@ -180,10 +164,10 @@ const config = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/LeXarDev"
       target="_blank"
       rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+    >LeXarDev</a> 💛`,
 };
 
 export default config;
